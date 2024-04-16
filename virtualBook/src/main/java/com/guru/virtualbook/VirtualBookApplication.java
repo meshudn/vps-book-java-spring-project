2e1fd49959ac2735c45e94defeb6e30b7f65bd55
meshudn <meshu.uiu@gmail.com>
2024-04-16 15:03:51 +0200

    init virtual book service

---
+++ virtualBook/src/main/java/com/guru/virtualbook/VirtualBookApplication.java
@@ -0,0 +1,13 @@
+package com.guru.virtualbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualBookApplication.class, args);
+	}
+
+}
---
+++ virtualBook/src/test/java/com/guru/virtualbook/VirtualBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.virtualbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
